3724213de40785bd9d2908292f7d159e73e9e06c
David Wright <wright.david.27@gmail.com>
2016-03-18 11:52:10 -0400

    First commit

---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+$helloWorld = function($genre) {
+    return "Hello world {$genre}";
+};
+
+Route::get('/', function () {
+    return View::make('simple');
+});
+
+Route::get('/{genre}', $helloWorld);
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
---
+++ resources/views/simple.php
@@ -0,0 +1,12 @@
+<!-- app/html/views/simple.php -->
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Simple View!</title>
+</head>
+<body>
+    <h1>Oh yeah! Views!</h1>
+</body>
+</html>
