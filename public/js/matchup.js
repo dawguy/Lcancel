2b0dd5cb1531de7bc30de37dc93fafe2662ce150
David Wright <wright.david.27@gmail.com>
2016-09-12 11:24:41 -0400

    Uses some real data now. But isn't meaningful, good start.

--- public/js/matchup.js
+++ public/js/matchup.js
@@ -2,13 +2,37 @@ $(document).ready(function(){
     init();
 });
 
+var matchupsBarData = [];
+
 function init(){
     console.log(matches);
     console.log(characters);
+
+    for(var i = 0; i < matches.length; i++){
+        var match = matches[i]
+        var character = characters[match['winner_character']];
+
+        if(typeof matchupsBarData[character] === 'undefined'){
+            matchupsBarData[character] = 1;
+        }
+        else {
+            matchupsBarData[character] += 1;
+        }
+    }
+
+    console.log(matchupsBarData);
+
+    var data = Object.keys(matchupsBarData)
+        .map(function(key){
+            return matchupsBarData[key]
+        });
+
     d3.select(".chart")
       .selectAll("div")
-        .data(matches)
+        .data(data)
       .enter().append("div")
         .style("width", function(d) { return d * 10 + "px"; })
+        .style("background-color", function(d) { return 'gray'; })
+        .style("color", function(d) { return 'black'; })
         .text(function(d) { return d; });
 }
