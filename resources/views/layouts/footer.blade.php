486d54776bc391e3a1d356f9d4070d1a17a045bd
vagrant <vagrant@homestead>
2017-06-20 01:36:16 +0000

    Added http to github link so it will properly redirect the browser

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-muted">
     <div class="container">
-        <p>Found a bug? Create a support issue at <a href="www.github.com/dawguy/lcancel">Lcancel's Github</a></p>
+        <p>Found a bug? Create a support issue at <a href="http://www.github.com/dawguy/lcancel">Lcancel's Github</a></p>
     </div>
 </footer>
